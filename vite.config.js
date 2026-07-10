a2472d22c09f45e17f25dfa26cb68b74d1aeba80
s5xv <saidsite13@icloud.com>
2026-07-10 14:16:07 +0300

    Simplify vite.config.js to fix build error

--- vite.config.js
+++ vite.config.js
@@ -7,21 +7,6 @@ export default defineConfig({
     target: 'esnext',
     minify: 'esbuild',
     cssMinify: true,
-    rollupOptions: {
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            if (id.includes('react') || id.includes('react-dom') || id.includes('react-router-dom')) {
-              return 'vendor';
-            }
-            if (id.includes('supabase')) {
-              return 'supabase';
-            }
-            return 'lib';
-          }
-        }
-      }
-    },
     chunkSizeWarningLimit: 1000
   },
   optimizeDeps: {
